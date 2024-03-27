e0c316888c5343eece08a4ac243ac3703434632b
vikas61 <81066106+vikas61@users.noreply.github.com>
2024-03-27 18:59:51 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,1 +1,37 @@
+const mongoose = require("mongoose");
 
+// Connnect to MongoDb
+
+mongoose.connect(process.env.MONGODB_URI);
+
+// Define Schema
+const AdminSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+});
+
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    purchasedCourses:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Course",
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    title:String,
+    description:String,
+    imageLink:String,
+    price:Number,
+});
+
+const Admin = mongoose.model('Admin',AdminSchema);
+const User = mongoose.model('User',userSchema);
+const Course = mongoose.model('Course',CourseSchema);
+
+module.exports ={
+    Admin,
+    User,
+    Course,
+}
