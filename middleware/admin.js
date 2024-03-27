ea7f4bc4b3e2f700623dc665083f1dba80e2be3e
vikas61 <81066106+vikas61@users.noreply.github.com>
2024-03-27 19:05:49 +0530

    admin.js

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,1 +1,23 @@
+const {Admin}=require("../db/index");
 
+// Middleware for handeling auth
+function adminMiddleware(req,res,next){
+    // iMPLEMENT ADMIN AUTH LOGIN
+    // You need to check the headers and validate the admin from the admin DB
+    const username=req.headers.username;
+    const password=req.headers.password;
+    Admin.findOne({
+        username:username,
+        password:password,
+    }).then(function(value){
+        if(value){
+            next();
+        }else{
+            res.status(403).json({
+                msg:"Admin doesn't exists"
+            })
+        }
+    })
+}
+
+module.exports = adminMiddleware;
