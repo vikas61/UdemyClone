4c7a04c6ef76f390d52dc99b51942fa1b463ca29
vikas61 <81066106+vikas61@users.noreply.github.com>
2024-03-27 19:08:14 +0530

    admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,49 @@
+const express = require("express");
+const adminMiddleware = require("../middleware/admin");
+const router = express.Router();
+const {Admin,Course} = require("../db/index");
 
+router.post("/signup",async (req,res)=>{
+    // Implement admin signup Logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // check if a user with this username already exists
+    await Admin.create({
+        username:username,
+        password:password,
+    })
+    res.json({
+        msg:"Admin created successfuly"
+    })
+})
+
+
+router.post("/courses",adminMiddleware,async (req,res)=>{
+    // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const newCourse = await Course.create({
+        title:title,
+        description:description,
+        imageLink:imageLink,
+        price:price,
+    })
+    res.json({
+        msg:"Course Successfully created",courseId:newCourse._id
+    })
+
+})
+
+router.get("/courses",adminMiddleware,async (req,res)=>{
+    // Implement fetching all courses logic
+    const allCourses = await Course.find({});
+    res.json({
+        courses:allCourses
+    })
+})
+
+module.exports = router;
